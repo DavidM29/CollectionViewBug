e5f24c5872f99c2f46661dc319631fac1c352762
DavidM29 <Davitm@bit.ge>
2024-03-28 11:15:58 +0400

    Add project files.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly ObservableCollection<string> _items = ["Item 1", "Item 2", "Item 3"];
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CollectionView1.ItemsSource = _items;
+            CollectionView2.ItemsSource = _items;
+        }
+    }
+
+}
